e146ee8b992241f7b51f9d488a6a98b6b0c3fab6
Chinomso Onyeukwu <40630918+2besweet@users.noreply.github.com>
2020-06-04 17:00:28 +0100

    Updated 3

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,9 +1,14 @@
 import React from "react";
 
+import C from "../images/c.png";
+
 const DashB = () => {
     return (
     <div className='tabDiv4'>
+        <div className='dashDiv1'>
+        <img className= "rec22Div" src={C}/>
         <h3 className='tabText'>Dashboard</h3>
+        </div>   
     </div>
     )
 }
